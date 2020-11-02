9f9d66f6aa59c3a8e698c28c05ed3486e4a44f1e
Ben Lehmann <62218426+BenLehmann12@users.noreply.github.com>
2020-11-02 12:00:55 -0600

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -6,7 +6,6 @@ class MyTestCase(unittest.TestCase):
         self.student = Students.Students('Lehmann','Ben', 'Math', gpa=3.0)
     def tearDown(self):
         del self.student
-
     def  test_object_created_required_attributes(self):
         self.assertEqual(self.student.first_name, 'Ben')
         self.assertEqual(self.student.last_name, 'Lehmann')
@@ -22,6 +21,9 @@ class MyTestCase(unittest.TestCase):
     def test_object_not_created_error_first_name(self):
         with self.assertRaises(ValueError):
             s = Students.Students('123', 'Lehmann', 'Math', 3.0)
+    def test_object_not_created_error_major(self):
+        with self.assertRaises(ValueError):
+            s = Students.Students('Lehmann','Ben','123',3.0)
 
 
 if __name__ == '__main__':
