77e9d336b1ae27a4660b93f9b3418939b245ccd1
Ben Lehmann <62218426+BenLehmann12@users.noreply.github.com>
2020-11-02 11:55:01 -0600

    Update test_Class.py

--- test_class.py
+++ test_class.py
@@ -3,7 +3,7 @@ from class_definitions import Student as Students
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.student = Students.Students('Lehmann','Ben', 'Math', gpa=3.8)
+        self.student = Students.Students('Lehmann','Ben', 'Math', gpa=3.0)
     def tearDown(self):
         del self.student
 
@@ -11,11 +11,11 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.student.first_name, 'Ben')
         self.assertEqual(self.student.last_name, 'Lehmann')
     def test_object_created_all_attributes(self):
-        student = Students.Students('Lehmann','Ben', 'Math', gpa=3.8)
+        student = Students.Students('Lehmann','Ben', 'Math', gpa=3.0)
         assert student.last_name == 'Lehmann'
         assert student.first_name == 'Ben'
         assert student.major == 'Math'
-        assert student.gpa == 3.8
+        assert student.gpa == 3.0
     def test_object_not_created_error_last_name(self):
         with self.assertRaises(ValueError):
             s = Students.Students('123','Ben','Math', 3.0)
