d9eada9a127039a2b6c485ed5dd1e26dda1369e8
Ben Lehmann <62218426+BenLehmann12@users.noreply.github.com>
2020-11-01 18:53:02 -0600

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -10,6 +10,12 @@ class MyTestCase(unittest.TestCase):
     def  test_object_created_required_attributes(self):
         self.assertEqual(self.student.first_name, 'Ben')
         self.assertEqual(self.student.last_name, 'Lehmann')
+    def test_object_created_all_attributes(self):
+        student = Students.Students('Lehmann','Ben', 'Math', gpa=3.8)
+        assert student.last_name == 'Lehmann'
+        assert student.first_name == 'Ben'
+        assert student.major == 'Math'
+        assert student.gpa == 3.8
 
 
 
